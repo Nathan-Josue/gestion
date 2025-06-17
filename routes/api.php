718c598ba1bd70141c4721a001a837435896648f
Nathan-Josue <nathan.nguessan.dev@gmail.com>
2025-06-17 01:40:41 +0000

    gestion de l'api

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,6 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('categories', CategoryController::class);
+Route::post('/test-categorie', function () {
+    return response()->json(['message' => 'ça marche !']);
+});
